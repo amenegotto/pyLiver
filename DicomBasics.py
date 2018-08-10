44da353bd027b1f4db64feb9f97a018eff56becb
Alan Baronio Menegotto <alan@menegotto.com>
2018-08-10 15:59:15 -0300

    View Images in Sequence

--- DicomBasics.py
+++ DicomBasics.py
@@ -1,22 +1,11 @@
 import os
 import pydicom
+from dcmfile import DcmFile
 from operator import itemgetter
 
 import matplotlib.pyplot as plt
 
-class DcmFile:
-    fileName = ""
-    order = 0
-
-    def __init__(self, fileName, order):
-        self.fileName = fileName
-        self.order = order
-
-    def __getitem__(self, item):
-        return self.fileName[item]
-
-    def __setitem__(self, key, value):
-
+# read basics
 #ds = pydicom.dcmread("dcm/000001.dcm")
 #print(ds.InstanceNumber)
 # check for CT
@@ -30,29 +19,22 @@ class DcmFile:
 #plt.show(block=True)
 
 
+# order the images
 images = []
-# read to check order
 for f in os.listdir("dcm/"):
     ds = pydicom.dcmread("dcm/" + f)
     images.append(DcmFile(f, ds.InstanceNumber))
-
-# order the images
-images.sort(key=lambda x:x[1])
-
-for i in images:
-    print(i.fileName + " " + str(i.order))
-
-
+s_images = sorted(images, key = lambda x: x.order)
 
 # animate image
-#img = None
-#for f in os.listdir("dcm/"):
-#    ds = pydicom.dcmread("dcm/" + f)
-#    if img is None:
-#        img = plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
-#    else:
-#        img.set_data(ds.pixel_array)
-#    plt.pause(.1)
-#    plt.draw()
+img = None
+for f in s_images:
+    ds = pydicom.dcmread("dcm/" + f.fileName)
+    if img is None:
+        img = plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
+    else:
+        img.set_data(ds.pixel_array)
+    plt.pause(.1)
+    plt.draw()
 
-#plt.show(block=True)
+plt.show(block=True)
