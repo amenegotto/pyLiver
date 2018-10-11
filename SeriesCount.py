cc67c04fb6b68f5fcedc1d894588f71fde592252
Alan Baronio Menegotto <alan@menegotto.com>
2018-10-10 22:05:01 -0300

    1st Dataset Cleanup

---
+++ SeriesCount.py
@@ -0,0 +1,27 @@
+import os
+
+# Count CT series and files recursively
+
+SRC_DIR="C:/Users/hp/Downloads/tcga-lihc/TCGA-LIHC_CT_DCM/"
+
+qtd = 0
+pathlist=[]
+
+for dirpath, dirs, files in os.walk(SRC_DIR):	 
+	path = dirpath.split('/')
+	
+	for f in files:
+		if os.path.splitext(f)[1] == ".dcm":
+                    qtd = qtd + 1
+
+                    if not(any(dirpath in s for s in pathlist)):
+                        pathlist.append(dirpath)
+
+
+print(pathlist[0])
+print(pathlist[1])
+print(pathlist[5])
+print("Files = ", qtd)
+print("Series = ", len(pathlist))
+                   
+
