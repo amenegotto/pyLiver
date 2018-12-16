d91219adcf5ea0e3630938a3d0d0700c57aad27c
Alan Baronio Menegotto <alan@menegotto.com>
2018-12-16 13:31:49 -0200

    Create Mono Image Dataset

--- CreateMonoDataset.py
+++ CreateMonoDataset.py
@@ -2,20 +2,36 @@ import os
 import pydicom
 import pywt
 import numpy as np
+import uuid
 from scipy.misc import imsave
 
 # PURPOSE: 
 # Create image dataset without pre-processing step (filters, denoising, etc.).
-# Just convert DICOM to PNG keeping the patient's reference (path) 
+# Just convert DICOM to PNG creating one UUID for each slice and storing this UUID on a CSV to keep track.
+# If needed, it can keep the patient's reference (path) 
 
-#SRC_DIR='C:/Users/hp/Downloads/tcga-lihc'
-SRC_DIR='/home/amenegotto/tcga-lihc'
-OUT_DIR='/tmp/tcga-lihc-png'
+SRC_DIR='C:/Users/hp/Downloads/tcga-lihc/TCGA-LIHC_CT_DCM'
+OUT_DIR='C:/Users/hp/Downloads/tcga-lihc/tcga-lihc-png'
+#SRC_DIR='/home/amenegotto/tcga-lihc'
+#OUT_DIR='/tmp/tcga-lihc-png'
+CSV_FILENAME='images-id-without-filters.csv'
 
-def Ct2Png(file_path, file_name):
-    out_path = file_path.replace(SRC_DIR, OUT_DIR)
+def create_image_id(file_path, file_name):
+    slice_id = uuid.uuid4().hex
+    path = file_path.split(os.sep)
+#    print(path, ',',file_name, ',', slice_id, path[1] + '_' + slice_id + '.png')
+    fname = path[1] + '_' + slice_id + '.png' 
+    with open(CSV_FILENAME, "a") as f:
+        f.write(', '.join(path) + ',' + file_name + ',' + slice_id + ',' + fname + '\n')
+    return fname
+
+def ct_to_png(file_path, file_name, keep_dir = True):
+    if (keep_dir):
+        out_path = file_path.replace(SRC_DIR, OUT_DIR)
+    else:
+        out_path = OUT_DIR + '/'
     os.makedirs(out_path, exist_ok=True)
-    dst_name = out_path + os.path.splitext(file_name)[0] + '.png'
+    dst_name = out_path + create_image_id(file_path, file_name)
     print(file_path, file_name, ' => ', dst_name)
 
     ds = pydicom.dcmread(file_path + file_name)
@@ -38,7 +54,8 @@ def Ct2Png(file_path, file_name):
 def dcm_dir_convert(inputdir):
     for f in os.listdir(inputdir):
         if not os.path.isdir(inputdir + f):
-           Ct2Png(inputdir, f) 
+           ct_to_png(inputdir, f, False)
+           create_image_id(inputdir, f)
 
 # search recursively for dcm directories
 pathlist=[]
