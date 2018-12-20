4a2327970e63abefe97f618ed6286238d1a050b0
alan@menegotto.com <alan@menegotto.com>
2018-12-20 16:39:22 -0200

    Random Forest Imputation Basics

--- ImputationDataPrepareInterpolation.py
+++ ImputationDataPrepareInterpolation.py
@@ -5,11 +5,11 @@ import pandas as pd
 # Here spline interpolation is used as imputation method
 # The base CSV used here is an ensemble of TCGA-LIHC clinical data and HCC Survival Dataset (UCI)
 
-df = pd.read_csv('csv/hcc-data.csv')
+df = pd.read_csv('csv/hcc-survive.csv')
 df['Alpha-Fetoprotein'] = df['Alpha-Fetoprotein'].interpolate(method='spline', order=1)
 df['Platelets'] = df['Platelets'].interpolate(method='spline', order=1)
 df['Albumin'] = df['Albumin'].interpolate(method='spline', order=1)
 df['Total Bilirubin'] = df['Total Bilirubin'].interpolate(method='spline', order=1)
 df['Creatinine'] = df['Creatinine'].interpolate(method='spline', order=1)
 
-df.to_csv('hcc-data-filled.csv')        
+df.to_csv('csv/hcc-data-filled.csv')        
--- RandomForestImputation.py
+++ RandomForestImputation.py
@@ -8,27 +8,47 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
-df = pd.read_csv('csv/hcc-data-filled.csv')
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('csv/hcc-data-best-features.csv')
 
 # here divides in labeles / target. In my case should do for each laboratory exam result
-#X = dataset.iloc[:, 0:4].values
-#y = dataset.iloc[:, 4].values
+X = df.iloc[:, df.columns != 'Alpha-Fetoprotein'].values
+y = df.iloc[:, 2].values
 
 
 # divides in test/validation with 80/20
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
 
 
 # the n_estimators value should be tested. Lower RMSE is better...
-regressor = RandomForestRegressor(n_estimators=20, random_state=0)
-regressor.fit(X_train, y_train)
+regressor = RandomForestRegressor(random_state=42)
+
+
+estimators = np.arange(10, 700, 10)
+scores = []
+for n in estimators:
+    regressor.set_params(n_estimators=n)
+    regressor.fit(X_train, y_train)
+    scores.append(regressor.score(X_test, y_test))
+plt.title("Effect of n_estimators")
+plt.xlabel("n_estimator")
+plt.ylabel("score")
+plt.plot(estimators, scores)
+plt.show(block=True)
+
 y_pred = regressor.predict(X_test)
+print("Input=%s, Predicted=%s" % (X_test[0], y_pred[0]))
 
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
-# create X_test array based on TCGA clinical data for data imputation
-# with one line and n columns
-predicted = rf.predict(X_test)
-print("Input=%s, Predicted=%s" % (X_test[0], predicted[0]))
+
+feature_list = list(df.iloc[:, df.columns != 'Alpha-Fetoprotein'].columns)
+
+# Get numerical feature importances
+importances = list(regressor.feature_importances_)
+
+# Print out the feature and importances
+print(importances)
