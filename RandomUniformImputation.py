c0fe4a05ad4c11b4dbd0d77ab53fbe1917dd9fd0
Alan Baronio Menegotto <alan@menegotto.com>
2018-12-26 21:39:50 -0200

    After Random Uniform Imputation

--- RandomUniformImputation.py
+++ RandomUniformImputation.py
@@ -1,6 +1,6 @@
 # PURPOSE:
 # Do Uniform distribution imputation on exams results based on references values grouped by age and sex
-# extracted from peer-reviewed scientific studies
+# extracted from peer-reviewed scientific articles and books.
 # It should be used when only age and gender columns are filled
 
 import numpy as np
@@ -10,11 +10,16 @@ import pandas as pd
 def calculate_value(d, exam, gender, age):
     for ir, rr in d[(d.Gender == gender) & (d.Exam == exam)].iterrows():
         if age >= rr['Min Age'] and age <= rr['Max Age']:
-            return np.random.normal(rr['Mean'], rr['Standard Deviation'], 10000)[0]
+            val = 0
+            while True:
+                val = np.random.normal(rr['Mean'], rr['Standard Deviation'], 10000)[5000]
+                if val > 0 and rr['Mean'] - rr['Standard Deviation'] <= val <= rr['Mean'] + rr['Standard Deviation']:
+                    break
+            return val
 
 
 df_rr = pd.read_csv('csv/reference-range.csv')
-df = pd.read_csv('csv/input.csv')
+df = pd.read_csv('csv/input-uniform.csv')
 
 for i, r in df.iterrows():
     df.at[i, 'AFP'] = calculate_value(df_rr, 'AFP', r['Gender'], r['Age'])
