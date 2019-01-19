48df0603eb90a816a6c16792c7bed431bdab686d
Alan Menegotto <alan@menegotto.com>
2019-01-19 22:01:58 +0000

    After first experiments

--- UnimodalCnn.py
+++ UnimodalCnn.py
@@ -11,29 +11,30 @@ from ExecutionAttributes import ExecutionAttribute
 from Summary import plot_train_stats, create_results_dir, get_base_name, write_summary_txt
 
 # Summary Information
-SUMMARY_PATH="c:/temp/results"
+SUMMARY_PATH="/mnt/data/results"
+#SUMMARY_PATH="c:/temp/results"
 # SUMMARY_PATH="/tmp/results"
 NETWORK_FORMAT="Unimodal"
 IMAGE_FORMAT="2D"
 SUMMARY_BASEPATH=create_results_dir(SUMMARY_PATH, NETWORK_FORMAT, IMAGE_FORMAT)
 
 # how many times to execute the training/validation/test
-CYCLES = 5
+CYCLES = 1
 
 #
 # Execution Attributes
 attr = ExecutionAttribute()
 
 # dimensions of our images.
-attr.img_width, attr.img_height = 64, 64
+attr.img_width, attr.img_height = 96, 96
 
 # network parameters
-attr.path='C:/Users/hp/Downloads/cars_train'
+#attr.path='C:/Users/hp/Downloads/cars_train'
 # attr.path='/home/amenegotto/dataset/2d/sem_pre_proc/'
-# attr.path='/mnt/data/image/2d/sem_pre_proc/'
+attr.path='/mnt/data/image/2d/sem_pre_proc/'
 attr.summ_basename=get_base_name(SUMMARY_BASEPATH)
 attr.epochs = 10
-attr.batch_size = 5
+attr.batch_size = 20
 attr.set_dir_names()
 
 if K.image_data_format() == 'channels_first':
@@ -44,17 +45,17 @@ else:
 for i in range(0, CYCLES):
     # define model
     attr.model = Sequential()
-    #attr.model.add(Conv2D(64, (3, 3), input_shape=input_s))
-    #attr.model.add(Activation('relu'))
-    #attr.model.add(Conv2D(64, (3, 3), input_shape=input_s))
-    #attr.model.add(Activation('relu'))
-    #attr.model.add(MaxPooling2D(pool_size=(3, 3)))
+#    attr.model.add(Conv2D(128, (3, 3), input_shape=input_s))
+#    attr.model.add(Activation('relu'))
+#    attr.model.add(Conv2D(128, (3, 3), input_shape=input_s))
+#    attr.model.add(Activation('relu'))
+#    attr.model.add(MaxPooling2D(pool_size=(3, 3)))
 
-    #attr.model.add(Conv2D(48, (3, 3), input_shape=input_s))
-    #attr.model.add(Activation('relu'))
-    #attr.model.add(Conv2D(48, (3, 3), input_shape=input_s))
-    #attr.model.add(Activation('relu'))
-    #attr.model.add(MaxPooling2D(pool_size=(3, 3)))
+    attr.model.add(Conv2D(64, (3, 3), input_shape=input_s))
+    attr.model.add(Activation('relu'))
+    attr.model.add(Conv2D(64, (3, 3), input_shape=input_s))
+    attr.model.add(Activation('relu'))
+    attr.model.add(MaxPooling2D(pool_size=(3, 3)))
 
     attr.model.add(Conv2D(32, (3, 3), input_shape=input_s))
     attr.model.add(Activation('relu'))
@@ -65,7 +66,7 @@ for i in range(0, CYCLES):
     attr.model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
     attr.model.add(Dense(32))
     attr.model.add(Activation('relu'))
-    attr.model.add(Dropout(0.1))
+    attr.model.add(Dropout(0.5))
     attr.model.add(Dense(1))
     attr.model.add(Activation('sigmoid'))
 
@@ -76,7 +77,7 @@ for i in range(0, CYCLES):
 
     # this is the augmentation configuration we will use for training
     train_datagen = ImageDataGenerator(
-            rotation_range=20,
+            rotation_range=10,
             width_shift_range=0.2,
             height_shift_range=0.2,
             rescale=1./255,
@@ -110,7 +111,6 @@ for i in range(0, CYCLES):
     # calculate steps based on number of images and batch size
     attr.calculate_steps()
 
-
     attr.increment_seq()
     history = attr.model.fit_generator(
         attr.train_generator,
@@ -121,4 +121,4 @@ for i in range(0, CYCLES):
 
     plot_train_stats(history, attr.curr_basename + '-training_loss.png', attr.curr_basename + '-training_accuracy.png')
 
-    write_summary_txt(attr, NETWORK_FORMAT, IMAGE_FORMAT, ['barato', 'caro'])
+    write_summary_txt(attr, NETWORK_FORMAT, IMAGE_FORMAT, ['positive', 'negative'])
