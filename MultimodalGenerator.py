23ff03cb861ad5713906abeec7459471901a39f5
alan@menegotto.com <alan@menegotto.com>
2019-04-16 16:15:17 -0300

    Image augmentation for MultimodalGenerator Draft

--- MultimodalGenerator.py
+++ MultimodalGenerator.py
@@ -4,11 +4,12 @@
 from keras.utils import Sequence
 from Datasets import get_image
 import numpy as np
-
+from ImageAugmentation import random_shift, random_rotation, random_shear, random_zoom
+import random
 
 class MultimodalGenerator(Sequence):
     
-    def __init__(self, npy_path, batch_size, height, width, channels, classes, should_shuffle=True, is_debug=False):
+    def __init__(self, npy_path, batch_size, height, width, channels, classes, should_shuffle=True, is_debug=False, width_shift=None, height_shift=None, rotation_angle=None, shear_factor=None, min_zoom=None, max_zoom=None):
         self.debug = is_debug
 
         if self.debug:
@@ -26,6 +27,12 @@ class MultimodalGenerator(Sequence):
         self.n_channels = channels
         self.n_classes = classes
         self.shuffle = should_shuffle
+        self.width_shift = width_shift
+        self.height_shift = height_shift
+        self.rotation_angle = rotation_angle
+        self.shear_factor = shear_factor
+        self.min_zoom = min_zoom
+        self.max_zoom = max_zoom
         self.on_epoch_end()
 
     def __len__(self):
@@ -57,7 +64,19 @@ class MultimodalGenerator(Sequence):
         batch_labels = batch_array[:, 21]
 
         for img_path in imgs_path:
-            img = get_image(img_path, self.width, self.height)            
+            img = get_image(img_path, self.width, self.height)
+
+            tr = random.randint(0,3)
+
+            if tr = 0 and (self.width_shift is not None or self.height_shift is not None):
+                img = random_shift(img, self.width_shift, self.height_shift)
+            elif tr = 1 and self.rotation_angle is not None:
+                img = random_rotation(img, self.rotation_angle)
+            elif tr = 2 and self.shear_factor is not None:
+                img = random_shear(img, self.shear_factor)
+            elif tr=3 and (self.min_zoom is not None or self.max_zoom is not None):
+                img = random_zoom(img, self.min_zoom, self.max_zoom)
+
             batch_img.append(img)
 
         X = [(np.array(batch_img, dtype="float") / 255.0), batch_attributes]
